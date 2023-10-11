022a17d85dbed05bf2b92fc5719d0c721fcf2bf4
AmaterasuM <anri.zubashvili@gmail.com>
2023-10-12 02:00:58 +0400

    day 4

---
+++ day 4/Homework/Homework.py
@@ -0,0 +1,24 @@
+#Users Age and Calculation 
+age = int(input("Enter your age: "))
+print(age)
+
+age = age * 7 / 3
+print(age)
+
+sum = age + (4 * 9)
+print(sum)
+
+#Car Price
+Old_price = 7000
+New_price = Old_price + Old_price * 0.62
+Price_increment = New_price - Old_price
+print("Old price was: " + str(Old_price) + "$. New price is: " + str(New_price) + "$. Price increased by: " + str(Price_increment)+ "$")
+
+
+#Anegdoti..
+print("Man gos the khinkali house")
+print("Waiter asks")
+khinkali = int(input("How much khinkali would you like? "))
+print("Waiter asks man why", khinkali,"and not",khinkali + 1,"?")
+print("The man says", khinkali + 1,"? Do i look like a pig?")
+print("Waiter looks at the man like: O.o")
