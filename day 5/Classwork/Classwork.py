d2a9843e6277b5b29f42bf7a859eb0b0a9daad3b
AmaterasuM <anri.zubashvili@gmail.com>
2023-10-12 02:01:36 +0400

    day 5

---
+++ day 5/Classwork/Classwork.py
@@ -0,0 +1,13 @@
+# User_name = input("Enter your name: ")
+# User_Age = int(input("enter your age: "))
+# Year = 2023
+
+# for i in range(10):
+#     print(User_name,"შენ იქნები", User_Age, "წლის" , Year, "წელს" )
+#     User_Age += 1
+#     Year += 1
+
+# seats = 10
+# while seats > 0:
+#     print("Seats available", seats)
+#     seats -= 1
