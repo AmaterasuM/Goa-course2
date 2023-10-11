a064d04ee7e16b4d0a7dd4dbf38256c1cfe784e2
AmaterasuM <anri.zubashvili@gmail.com>
2023-10-12 01:58:36 +0400

    day 2

---
+++ day 2/Classwork/classwork1.py
@@ -0,0 +1,7 @@
+mom_age = 40
+dad_age = 43
+sister_age = 19
+
+print("In 5 years My mom's age will be",mom_age + 5)
+print("In 5 years My dad's age will be ",dad_age + 5)
+print("In 5 years My sister's age will be ",sister_age + 5)
