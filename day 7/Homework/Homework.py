ad69acccfd92d4c09b88474c66ffb32d20796cd4
AmaterasuM <anri.zubashvili@gmail.com>
2023-10-14 12:29:22 +0400

    day 7

---
+++ day 7/Homework/Homework.py
@@ -0,0 +1,14 @@
+#homework 1
+leader_names = ["gabrieli molodini","dato janezashvili"," dato Tyeshelashvili", "filiashvili"]
+print(leader_names[1])
+
+
+#homweork 2
+name =["Luka", "luka chxitunidze", "dachi vashagashvili", "luka kechexmadze", "noi comaia", "cotne sartania", "giorgi gorgiladze", "dachi abashidze", "luka suxiashvili", "luka siradze", "misho chubinidze", "gigi gabitadze", "ako mitiashvili", "dito muladze", "omiko mshvildadze", "nika bregadze", "tato chogovade"]
+
+print(name[5:10])
+
+#homework 3 
+fruits = ["vashli", "yurdzeni", "banani", "stafilo"]
+fruits[3] = "msxali"
+print(fruits)
