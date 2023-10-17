8bcb7b71eb73abc397b985127007f4cef597b90f
AmaterasuM <anri.zubashvili@gmail.com>
2023-10-17 23:32:33 +0400

    day 8

--- day 6/Homework/Homework.py
+++ day 6/Homework/Homework.py
@@ -5,8 +5,9 @@ for i in range(5):
     total += num
     total_nums += 1
 
-print(total / total_nums)
-Average_arithmetic = total / total_nums
+
+print(total // total_nums)
+Average_arithmetic = total // total_nums
 if Average_arithmetic > 9:
     print("გილოცავ მატრიცელო შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რასაც შეალიე შენი ცხოვრების წლები")
 elif Average_arithmetic < 5:
@@ -18,11 +19,24 @@ elif Average_arithmetic > 10 or Average_arithmetic < 0:
 
 
 
+
 salary = int(input("Enter your salary: "))
 
 if salary > 10000:
-    print("გო ა-ში სწავლ ობდ ი?")
+    print("გო ა ში სწავლ ობდ ი?")
 elif salary > 1000 and salary < 10000:
     print("you mid")
 else:
     print("შემოსუ ლ იყავი გო აში, მატ რიცელ ო.")
+
+
+
+
+compositions = [
+    "Simphony 5",
+    "Simphony 3",
+    "Simphony 7"
+]
+
+for i in compositions:
+    print(compositions)
---
+++ day 8/Homework/Homework.py
@@ -0,0 +1,16 @@
+#homework 1
+List1 = [65, 43, 16, 84, 21, 24, 86]
+sum = 0
+for i in List1:
+    sum += i
+print(sum)
+
+
+
+# homework 2
+List_1 = [65, 43, 16, 84, 21, 24, 86]
+for i in List_1:
+    if i % 2 == 0:
+        print(i,"Even")
+    else:
+        print(i,"Odd")
